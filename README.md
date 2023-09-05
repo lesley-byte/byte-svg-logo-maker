7d5fcf686efd23e38d874122e702749b00a02d90
Lesley-Byte <phosphorescence3@hotmail.com>
2023-09-05 14:29:50 -0400

    Added html file, dist folder, example logo,updated README.md and javascript in index.js and lib/shapes.js

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 # byte-svg-logo-maker
+
 A node.js command-line application that takes user input to generate a logo and save it as an SVG file.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+1. Clone the repo
+   ```sh
+   git clone
+    ```
+2. Navigate to the project directory
+   ```sh
+   cd byte-svg-logo-maker
+   ```
+3. Install NPM packages
+   ```sh
+    npm install
+    ```
+
+## Usage
+
+1. Run the application
+   ```sh
+   npm start
+   ```
+2. Follow the on-screen prompts to select colors, shapes, and text for your logo. After completing the prompts, an SVG file named logo.svg will be generated in the dist directory.
+
+## Features
+
+- Generate SVG logos with custom text, colors, and shapes.
+- Choose from three different shapes: Circle, Triangle, or Square.
+- Add custom text up to three characters.
+- Specify colors using either color keywords or hexadecimal values.
+
+## Tests
+
+1. Run the tests
+   ```sh
+   npm test
+   ```
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are greatly appreciated.
+
+1. Fork the repo
+2. Clone your forked repository to your machine.
+3. Create a new branch.
+4. Make your changes.
+5. Push your changes to your forked repository.
+6. Create a Pull Request to this repository.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
--- index.js
+++ index.js
@@ -1,19 +1,94 @@
-// command-line application that takes in user input to generate a logo and save it as an SVG file. The application prompts the user to select a color and shape, provide text for the logo, and save the generated SVG to a .svg file.
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
+// Importing required modules
 const inquirer = require("inquirer");
 const fs = require("fs");
 const shapes = require("./lib/shapes");
+
+// Array of questions for user prompts
+const questions = [
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to three characters for your logo:",
+    // Validation to ensure the entered text is up to three characters
+    validate: function (value) {
+      if (value.length > 3) {
+        return "Please enter up to three characters.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter a color keyword (OR a hexadecimal number) for your text:",
+    // Validation to ensure a color keyword or hex value is provided
+    validate: function (value) {
+      if (value.length < 1) {
+        return "Please enter a color keyword (OR a hexadecimal number).";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Select a shape for your logo:",
+    choices: ["Circle", "Triangle", "Square"], // Choices for the shapes
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter a color keyword (OR a hexadecimal number) for your shape:",
+    // Validation to ensure a color keyword or hex value is provided
+    validate: function (value) {
+      if (value.length < 1) {
+        return "Please enter a color keyword (OR a hexadecimal number).";
+      }
+      return true;
+    },
+  },
+];
+
+/**
+ * Function to write data to a file
+ * @param {string} fileName - The name of the file to be written to.
+ * @param {string} data - The data to be written.
+ */
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Generated " + fileName); // Notify that the file has been generated
+  });
+}
+
+/**
+ * Initialize the command-line application. 
+ * - Prompts the user to answer a set of questions.
+ * - Generate an SVG logo based on the answers.
+ * - Writes the SVG content to a file.
+ */
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    const shape = new shapes[response.shape]();
+    shape.setColor(response.shapeColor);
+    shape.setText(response.text);
+    shape.setTextColor(response.textColor);
+
+    // Wrap the shape's render output in an SVG root element
+    const svgContent = `
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+${shape.render()}
+</svg>
+        `;
+
+    writeToFile("dist/logo.svg", svgContent.trim()); // Save the SVG content to 'dist/logo.svg'
+  });
+}
+
+// Start the application
+init();
+
+// Export the init function for potential external use
+module.exports = init;
